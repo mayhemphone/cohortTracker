6b782705d243804188351ee51b1de86c526fb543
mayhem <zac@assembleinc.com>
2021-12-12 15:57:00 -0800

    cleanup

--- app/users/mutations/createUser.ts
+++ app/users/mutations/createUser.ts
@@ -3,7 +3,8 @@ import db from "db"
 import { z } from "zod"
 
 const CreateUser = z.object({
-  name: z.string(),
+  id: z.number(),
+  email: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreateUser), resolver.authorize(), async (input) => {
